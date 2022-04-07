53d845e9136f5b413d2baeb3ec5e4b1b6899081e
Armando Carera <armanxzmc@hotmail.com>
2022-04-07 12:27:22 -0400

    ui done

--- index.js
+++ index.js
@@ -3,64 +3,90 @@ const computerPlay = () => {
   const randomWeapon = Math.trunc(Math.random() * rockPaperScissors.length);
   return rockPaperScissors[randomWeapon];
 };
+let computerScore = 0;
+let playerScore = 0;
+const playerScoreP = document.querySelector(".player-score");
+const computerScoreP = document.querySelector(".computer-score");
 
-function game() {
-  let computerScore = 0;
-  let playerScore = 0;
-  function playRound(playerSelection, computerSelection) {
-    const playerSelect = playerSelection.toLowerCase();
-    if (playerSelect === "rock") {
-      if (computerSelection === "rock") {
-        return "It's a tie!";
-      } else if (computerSelection === "paper") {
-        computerScore++;
-        return "You Lose! Paper Beats Rock!";
-      } else if (computerSelection === "scissors") {
-        playerScore++;
-        return "You Win! Rock Beats Scissors!";
-      }
-    } else if (playerSelect === "paper") {
-      if (computerSelection === "paper") {
-        return "It's a tie!";
-      } else if (computerSelection === "scissors") {
-        computerScore++;
-        return "You Lose! Scissors Beats Paper!";
-      } else if (computerSelection === "rock") {
-        playerScore++;
-        return "You Win! Paper Beats Rock!";
-      }
-    } else if (playerSelect === "scissors") {
-      if (computerSelection === "scissors") {
-        return "It's a tie!";
-      } else if (computerSelection === "rock") {
-        computerScore++;
-        return "You Lose! Rock Beats Scissors!";
-      } else if (computerSelection === "paper") {
-        playerScore++;
-        return "You Win! Scissors Beats Paper!";
-      }
-    } else {
-      return "Please enter a weapon ranging from Rock, Paper, or Scissors";
+const playerScoreSpan = document.querySelector(".player");
+const computerScoreSpan = document.querySelector(".computer");
+const results = document.querySelector(".results");
+
+const buttons = document.querySelectorAll(".weapon");
+
+function playRound(playerSelection, computerSelection) {
+  const playerSelect = playerSelection.toLowerCase();
+  if (playerSelect === "rock") {
+    if (computerSelection === "rock") {
+      results.textContent = "It's a tie!";
+    } else if (computerSelection === "paper") {
+      computerScore++;
+      computerScoreSpan.textContent = computerScore;
+      results.textContent = "You Lose! Paper Beats Rock!";
+      return endGame();
+    } else if (computerSelection === "scissors") {
+      playerScore++;
+      playerScoreSpan.textContent = playerScore;
+      results.textContent = "You Win! Rock Beats Scissors!";
+      return endGame();
     }
+  } else if (playerSelect === "paper") {
+    if (computerSelection === "paper") {
+      results.textContent = "It's a tie!";
+    } else if (computerSelection === "scissors") {
+      computerScore++;
+      computerScoreSpan.textContent = computerScore;
+      results.textContent = "You Lose! Scissors Beats Paper!";
+      return endGame();
+    } else if (computerSelection === "rock") {
+      playerScore++;
+      playerScoreSpan.textContent = playerScore;
+      results.textContent = "You Win! Paper Beats Rock!";
+      return endGame();
+    }
+  } else if (playerSelect === "scissors") {
+    if (computerSelection === "scissors") {
+      results.textContent = "It's a tie!";
+    } else if (computerSelection === "rock") {
+      computerScore++;
+      computerScoreSpan.textContent = computerScore;
+      results.textContent = "You Lose! Rock Beats Scissors!";
+      return endGame();
+    } else if (computerSelection === "paper") {
+      playerScore++;
+      playerScoreSpan.textContent = playerScore;
+      results.textContent = "You Win! Scissors Beats Paper!";
+      return endGame();
+    }
+  } else {
+    results.textContent =
+      "Please enter a weapon ranging from Rock, Paper, or Scissors";
   }
-  return function () {
-    while (computerScore < 5 && playerScore < 5) {
-      const playerWeapon = prompt("Throw your weapon!");
-      console.log(playRound(playerWeapon, computerPlay()));
-    }
-
-    if (computerScore === 5) {
-      return alert(
-        `You may have won a few battles, but you ultimately lost the war :(. The computer wins. You: ${playerScore}; Computer: ${computerScore}`
-      );
-    } else {
-      return alert(
-        `You have Won! Wow, you beat a computer, congrats :). You: ${playerScore}; Computer: ${computerScore}`
-      );
-    }
-  };
 }
 
-const gameStarter = game();
-
-gameStarter();
+function endGame() {
+  if (computerScore === 5 || playerScore === 5) {
+    buttons.forEach((button) => (button.disabled = true));
+    if (computerScore === 5) {
+      if (playerScore > 0) {
+        results.textContent =
+          "You may have won a few battles but ultimately lost the war :(";
+      } else {
+        results.textContent = "Damn you got demolished bud. Get Fucked.";
+      }
+    } else if (playerScore === 5) {
+      if (computerScore > 0) {
+        results.textContent =
+          "You won the war but i bet the computer had you scared for a minute there :)";
+      } else {
+        results.textContent =
+          "Damn you fucked the computer. Good Job bud. Heres a cookie, you deserve it";
+      }
+    }
+  }
+}
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playRound(button.textContent, computerPlay());
+  });
+});
